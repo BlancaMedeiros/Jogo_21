c4fe26776c761412f09a20ba2d0de9e2c6be3d5b
BlancaMedeiros <biancamedeiroslabiapari@gmail.com>
2025-08-15 19:35:39 -0300

    Refactor: otimização da lógica de pontuação.

--- jogo_21.py
+++ jogo_21.py
@@ -1,33 +1,26 @@
 import random
 import os
 
+def obter_score_total_jogador (nome):
+    deseja_continuar = True
+    total_jogador = 0
+    while deseja_continuar and total_jogador < 21:
+        os.system('cls')
+        numero_sorteado = random.randrange(1, 10)
+        total_jogador = total_jogador + numero_sorteado
+        print("jogador:", nome,"\nO número sorteado foi:", numero_sorteado, "\n A sua pontuação total, por enquanto é:", total_jogador)
+        if total_jogador <= 21:
+            deseja_continuar = input ("Deseja sortear mais um número? s/n ") == "s"
+    if total_jogador > 21:
+        print("VOCÊ PERDEU, PORQUE PASSOU DE 21")
+    return total_jogador
+
 jogador1 = input("Digite o nome do primeiro jogador: ")
 jogador2 = input("Digite o nome do segundo jogador: ")
 
-total_jogador1 = 0 
-total_jogador2 = 0 
-deseja_continuar = True
+total_jogador1 = obter_score_total_jogador(jogador1)
+total_jogador2 = obter_score_total_jogador(jogador2)
 
-while deseja_continuar and total_jogador1 < 21:
-    os.system('cls')
-    numero_sorteado = random.randrange(1, 10)
-    total_jogador1 = total_jogador1 + numero_sorteado
-    print("jogador:", jogador1,"\nO número sorteado foi:", numero_sorteado, "\n A sua pontuação total, por enquanto é:", total_jogador1)
-    if total_jogador1 <= 21:
-        deseja_continuar = input ("Deseja sortear mais um número? s/n ") == "s"
-if total_jogador1 > 21:
-    print("VOCÊ PERDEU, PORQUE PASSOU DE 21")
-else:
-    deseja_continuar = True
-    while deseja_continuar and total_jogador2 < 21:
-        os.system('cls')
-        numero_sorteado = random.randrange(1, 10)
-        total_jogador2 = total_jogador2 + numero_sorteado
-        print("jogador:", jogador2, "\nO número sorteado foi:", numero_sorteado, "\n A sua pontuação total, por enquanto é:", total_jogador2)
-        if total_jogador2 <= 21:
-            deseja_continuar = input ("Deseja sortear mais um número? s/n ") == "s" 
-    if total_jogador2 > 21:
-        print("VOCÊ PERDEU, PORQUE PASSOU DE 21")
 if total_jogador1 < 21 and total_jogador2 < 21:
     if total_jogador1 == total_jogador2:
         print("Como o total dos dois jogadores foi igual, então temos um empate!")

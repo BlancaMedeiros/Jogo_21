b6c41788637c6d5ad6ad94da46b759b2b5cfd8ba
BlancaMedeiros <biancamedeiroslabiapari@gmail.com>
2025-08-15 18:47:44 -0300

    lógica de ganhadores

--- jogo_21.py
+++ jogo_21.py
@@ -13,7 +13,7 @@ while deseja_continuar and total_jogador1 < 21:
     numero_sorteado = random.randrange(1, 10)
     total_jogador1 = total_jogador1 + numero_sorteado
     print("jogador:", jogador1,"\nO número sorteado foi:", numero_sorteado, "\n A sua pontuação total, por enquanto é:", total_jogador1)
-    if total_jogador1 < 21:
+    if total_jogador1 <= 21:
         deseja_continuar = input ("Deseja sortear mais um número? s/n ") == "s"
 if total_jogador1 > 21:
     print("VOCÊ PERDEU, PORQUE PASSOU DE 21")
@@ -24,10 +24,16 @@ else:
         numero_sorteado = random.randrange(1, 10)
         total_jogador2 = total_jogador2 + numero_sorteado
         print("jogador:", jogador2, "\nO número sorteado foi:", numero_sorteado, "\n A sua pontuação total, por enquanto é:", total_jogador2)
-        if total_jogador2 < 21:
+        if total_jogador2 <= 21:
             deseja_continuar = input ("Deseja sortear mais um número? s/n ") == "s" 
     if total_jogador2 > 21:
         print("VOCÊ PERDEU, PORQUE PASSOU DE 21")
-
+if total_jogador1 < 21 and total_jogador2 < 21:
+    if total_jogador1 == total_jogador2:
+        print("Como o total dos dois jogadores foi igual, então temos um empate!")
+    elif total_jogador2 > total_jogador1:
+        print("Jogador: ", jogador2, "Você ganhou, com o total de ", total_jogador2, "pontos!")
+    else:
+        print("Jogador: ", jogador1, "Você ganhou, com o total de ", total_jogador1, "pontos!")
 
 
